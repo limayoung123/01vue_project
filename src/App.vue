0f3961c6418fb12a66b5a0935ce1eb0430528b1d
limayoung123 <nabaog123@naver.com>
2021-06-09 14:03:22 +0900

    component하는방법

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<ul class="view">
+  <li v-for="(item,i ) in product" :key="i">
+    {{product[i].title}} <!-- .하고 title을 넣으면 이름만 나오게된다 -->
+
+    </li>
+    
+</ul>
+<banner />
+<banner></banner>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vdata from './data.js'
+import 명작 from './components/banner.vue'
+// 값을 가져와야할 때 공식 import 이름 from '가져 올 위치'
+//data.js 문서 첫번째에는 export default(띄어쓰기)값을 넣어줘야함 (보내다)
+//이름 안에는 데이터이므로 return 안에 넣어줘야함
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      product:vdata,
+      
+    }
+  },
+  components:{
+    banner:명작
   }
+  
 }
+//name은 마음대로 작성하면됨
+//data는 함수형태이므로 (){}안에 작성해야함
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{margin:0; padding: 0; box-sizing: border-box;}
+  li{list-style: none;}
+  
 </style>
---
+++ src/components/banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="banner">banner</div>
+</template>
+
+<script>
+export default {
+    name:'banner',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+    .banner{width: 100%;height:200px;background: #ccc;
+    display:flex;justify-content: center; align-items: center;}
+</style>
